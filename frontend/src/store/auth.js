4c6dad11ad9d95edb564f0c3c7ddaeebe212cd0e
Aakash <aakashjha343@gmail.com>
2025-05-01 02:14:48 +0530

    added auth.js

---
+++ frontend/src/store/auth.js
@@ -0,0 +1,27 @@
+import { create } from 'zustand';
+import { mountStoreDevtool } from 'simple-zustand-devtools';
+
+const useAuthStore = create((set, get) => ({
+  allUserData: null,
+  loading: false,
+
+  setUser: (user) => set({ allUserData: user }),
+  setLoading: (loading) => set({ loading }),
+
+  user: () => {
+    const data = get().allUserData;
+    return {
+      user_id: data?.user_id || null,
+      username: data?.username || null,
+    };
+  },
+  
+  isLoggedIn: () => get().allUserData !== null,
+}));
+
+if (import.meta.env.DEV) {
+  mountStoreDevtool('Store', useAuthStore);
+}
+
+export { useAuthStore };
+
