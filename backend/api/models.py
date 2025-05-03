116e5de35bef5d56751896e76d39a20905fc31ff
Aakash <aakashjha343@gmail.com>
2025-05-03 23:52:21 +0530

    setup the db for the nested comments

--- backend/api/models.py
+++ backend/api/models.py
@@ -126,15 +126,14 @@ class Comment(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     comment = models.TextField()
-    reply = models.TextField(null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')  # For nested replies
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.post.title} - {self.name}"
-    
-    class Meta:
-        verbose_name_plural = "Comment"
 
+    class Meta:
+        verbose_name_plural = "Comments"
 
 class Bookmark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
