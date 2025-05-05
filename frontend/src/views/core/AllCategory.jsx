8c7cbd11c485cac2235315f6242bc4e601527df1
Aakash <aakashjha343@gmail.com>
2025-05-06 01:34:00 +0530

    Category client code completed

---
+++ frontend/src/views/core/AllCategory.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+
+import Header from '../partials/Header'
+import Footer from '../partials/Footer'
+import apiInstance from "../../utils/axios";
+
+
+const AllCategory = () => {
+    const [category, setCategory] = useState([]);
+    const fetchCategory = async () => {
+        const response = await apiInstance.get(`post/category/list/`);
+        setCategory(response.data);
+    };
+
+    useEffect(() => {
+        fetchCategory();
+    }, []);
+
+
+
+    return (
+        <>
+            <Header />
+
+            <div className="bg-light pt-5 pb-5 mb-3 mt-3">
+                <div className="container">
+                    <div className="row g-0">
+                        <div className="col-12 ">
+                            <div className="mb-4">
+                                <h2>Categories</h2>
+                            </div>
+                            <div className="d-flex flex-wrap justify-content-between ">
+                                {category?.map((c, index) => (
+                                    <div className="mt-2  " key={index}>
+                                        <Link to={`/category/${c.slug}/`} className="text-decoration-none text-dark">
+                                            <div className="card bg-transparent ">
+                                                <img className="card-img" src={c.image} style={{ width: "150px", height: "80px", objectFit: "cover" }} alt="card image" />
+                                                <div className="d-flex flex-column align-items-center mt-3 pb-2">
+                                                    <h5 className="mb-0 ">{c.title}</h5>
+                                                    <small>{c.post_count} Articles</small>
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
+
+        </>
+    )
+}
+
+export default AllCategory
